f3c0ab7e499ea3b36e21fa1f7797a438e5a80cc1
merlin0551 <merlinpynadath0551@gmail.com>
2024-12-17 11:46:30 +0530

    project updated

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.movieapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et1,et2;
+    Button b1;
+    String getUname,getPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        et1=(EditText) findViewById(R.id.uname);
+        et2=(EditText) findViewById(R.id.passwrd);
+        b1=(Button) findViewById(R.id.login);
+
+        getUname=et1.getText().toString();
+        getPassword=et2.getText().toString();
     }
 }
